385d3777a963f3758f65bdace50fcef7afa8d21c
saba2019 <saba.jnu08@gmail.com>
2019-03-13 21:36:25 +0530

    Fixed issue with query

--- 508816-Hackathon-report-service/src/main/java/com/cts/ora/report/fetch/dao/GeoMetricsDaoImpl.java
+++ 508816-Hackathon-report-service/src/main/java/com/cts/ora/report/fetch/dao/GeoMetricsDaoImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.slf4j.Logger;
@@ -30,18 +31,18 @@ public class GeoMetricsDaoImpl implements GeoMetricsDao {
 			Long ascId) {
 		List<GeoMetrics> metricsDate=null;
 		try {
-			String sql ="select period,location as locId, sum(eventt) as headCount,count(assc) as uniqueVolunteers,sum(vh) as volunteerHours,sum(th) as travelHours,eventt as totalEvents,on_weekend as isWeekend" + 
+			String sql ="select 1 as id,period,location as loc_id, sum(eventt) as head_count,count(assc) as uni_volunteers,sum(vh) as vol_hours,sum(th) as trav_hours,eventt as total_events,on_weekend as is_weekend " + 
 					"from (select E.asc_id as assc,EI.LOC_ID as location,count(E.event_id) as eventt,sum(E.vol_hours) as vh,sum(E.travel_hours) as th,EI.period ,EI.on_weekend " + 
-					"from ora_outreach_associate_event_map as E inner join ora_outreach_event_info as EI " + 
-					"on E.event_id=EI.event_id group by assc, location, on_weekend having E.asc_id=:ascId " + 
-					") as t group by period,location having period between :startPeriod and : endPeriod ";
-			TypedQuery<GeoMetrics> query=em.createNamedQuery(sql, GeoMetrics.class).setParameter("startPeriod", startPeriod)
+					"from ora_outreach_associate_event_map as E inner join ora_outreach_event_info as EI having E.asc_id=:ascId " + 
+					"on E.event_id=EI.event_id group by assc, location, on_weekend " + 
+					") as t group by period,location having period between :startPeriod and :endPeriod ";
+			em = emf.createEntityManager();
+			Query query=em.createNativeQuery(sql, GeoMetrics.class).setParameter("startPeriod", startPeriod)
 					.setParameter("endPeriod", endPeriod).setParameter("ascId", ascId);
 			if(!CollectionUtils.isEmpty(locIds)) {
 				sql=sql+ "and location in :locIds";
 				query.setParameter("locIds", locIds);
 			}
-			em = emf.createEntityManager();
 			metricsDate=query.getResultList();
 		} catch (Exception e) {
 			logger.error("Error in getGeoMetricsForUser:"+e.getMessage());
@@ -58,18 +59,19 @@ public class GeoMetricsDaoImpl implements GeoMetricsDao {
 	public List<GeoMetrics> getGeoMetrics(Integer startPeriod, Integer endPeriod, List<Integer> locIds) {
 		List<GeoMetrics> metricsDate=null;
 		try {
-			String sql ="select period,location as locId, sum(eventt) as headCount,count(assc) as uniqueVolunteers,sum(vh) as volunteerHours,sum(th) as travelHours,eventt as totalEvents,on_weekend as isWeekend" + 
+			String sql ="select 1 as id,period,location as loc_id, sum(eventt) as head_count,count(assc) as uni_volunteers,sum(vh) as vol_hours,sum(th) as trav_hours,eventt as total_events,on_weekend as is_weekend " + 
 					"from (select E.asc_id as assc,EI.LOC_ID as location,count(E.event_id) as eventt,sum(E.vol_hours) as vh,sum(E.travel_hours) as th,EI.period ,EI.on_weekend " + 
 					"from ora_outreach_associate_event_map as E inner join ora_outreach_event_info as EI " + 
 					"on E.event_id=EI.event_id group by assc, location, on_weekend " + 
-					") as t group by period,location having period between :startPeriod and : endPeriod ";
-			TypedQuery<GeoMetrics> query=em.createNamedQuery(sql, GeoMetrics.class).setParameter("startPeriod", startPeriod)
+					") as t group by period,location having period between :startPeriod and :endPeriod ";
+			em = emf.createEntityManager();
+			Query query=em.createNativeQuery(sql, GeoMetrics.class).setParameter("startPeriod", startPeriod)
 					.setParameter("endPeriod", endPeriod);
 			if(!CollectionUtils.isEmpty(locIds)) {
 				sql=sql+ "and location in :locIds";
 				query.setParameter("locIds", locIds);
 			}
-			em = emf.createEntityManager();
+			
 			metricsDate=query.getResultList();
 		} catch (Exception e) {
 			logger.error("Error in getGeoMetricsForUser:"+e.getMessage());
